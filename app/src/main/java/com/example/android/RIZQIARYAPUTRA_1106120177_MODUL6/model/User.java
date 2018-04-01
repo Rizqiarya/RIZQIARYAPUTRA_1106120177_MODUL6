a5814c2a75631589370fa738ff15eb4dac2b9882
HP <rrizqiarya@gmaul.com>
2018-04-01 21:32:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/RIZQIARYAPUTRA_1106120177_MODUL6/model/User.java
@@ -0,0 +1,29 @@
+package com.example.android.RIZQIARYAPUTRA_1106120177_MODUL6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
